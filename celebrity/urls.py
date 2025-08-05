9a62c0df964c64ccdc4f3995188290f8bb9aa34d
pract <kimc570612@gmail.com>
2025-08-05 20:57:46 +0900

    'eifjei'

--- celebrity/urls.py
+++ celebrity/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from celebrity import view
+from celebrity import views
 
 urlpatterns = [
-    path("<int:celebrity_id>/", view.celebrity_view, name="celebrity_view"),
-    path('celebrity_audio/', view.celebrity_audio, name='celebrity_audio'),
-    path('celebrity_response/<int:celebrity_id>/', view.celebrity_response, name='celebrity_response'),
+    path("<int:celebrity_id>/", views.celebrity_view, name="celebrity_view"),
+    path('celebrity_audio/', views.celebrity_audio, name='celebrity_audio'),
+    path('<int:celebrity_id>/response/', views.celebrity_response, name='celebrity_response'),
 ]
