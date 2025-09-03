9e5e70281e6f8ec6654189fa18d97ad0dbe45665
pract <kimc570612@gmail.com>
2025-09-03 23:54:58 +0900

    'geapungsin'

--- galatea/settings.py
+++ galatea/settings.py
@@ -56,7 +56,7 @@ LOCALE_PATHS = [
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '54.173.112.53',
