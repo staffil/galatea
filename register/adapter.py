75d8981bd2ad53f2827d90fece0fbd6a465e22dc
pract <kimc570612@gmail.com>
2025-09-04 12:03:51 +0900

    'WTF'

--- register/adapter.py
+++ register/adapter.py
@@ -4,15 +4,20 @@ from allauth.socialaccount.models import SocialApp
 from django.conf import settings
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_app(self, request, provider, client_id =None):
+
+    def get_app(self, request, provider, **kwargs):
+        """
+        Provider와 SITE_ID로 SocialApp을 찾음.
+        중복이 있어도 첫 번째 앱만 반환.
+        """
         site_id = getattr(settings, "SITE_ID", 1)
         apps = SocialApp.objects.filter(provider=provider, sites__id=site_id)
         if apps.exists():
-            return apps.first()  # 첫 번째 SocialApp만 사용
+            return apps.first()
         return None
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
-class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request, sociallogin):
-        # True이면 자동으로 회원가입 가능
+        """
+        True이면 자동으로 회원가입 가능
+        """
         return True
