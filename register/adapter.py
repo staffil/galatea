44208b3228813976c2e5d44227ff7b029d3a1daf
pract <kimc570612@gmail.com>
2025-09-04 11:22:20 +0900

    'sibal'

--- register/adapter.py
+++ register/adapter.py
@@ -4,7 +4,7 @@ from allauth.socialaccount.models import SocialApp
 from django.conf import settings
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_app(self, request, provider):
+    def get_app(self, request, provider, client_id =None):
         site_id = getattr(settings, "SITE_ID", 1)
         apps = SocialApp.objects.filter(provider=provider, sites__id=site_id)
         if apps.exists():
