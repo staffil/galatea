048af84ad7a8d1093b33d0927af30d36b5e8f6a1
pract <kimc570612@gmail.com>
2025-09-12 19:08:08 +0900

    'efefef'

--- static/js/home/voice_all.js
+++ static/js/home/voice_all.js
@@ -34,25 +34,3 @@ window.saveVoice = function(celebrityId){
 }
 
 
-// voice_all.js
-window.toggleAudio = function(element) {
-    const audio = element.closest('.voice-wrapper').querySelector('audio');
-    if (!audio) return;
-
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
-};
-
-window.seekAudio = function(event, progressContainer) {
-    const audio = progressContainer.closest('.voice-wrapper').querySelector('audio');
-    if (!audio) return;
-
-    const rect = progressContainer.getBoundingClientRect();
-    const clickX = event.clientX - rect.left;
-    const width = rect.width;
-    const percent = clickX / width;
-    audio.currentTime = percent * audio.duration;
-};
