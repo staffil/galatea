59ac8cc3e4f1856a38d06b99c8a5f67b6fa9f9e7
pract <kimc570612@gmail.com>
2025-09-13 22:21:03 +0900

    'fefefe'

--- galatea/settings.py
+++ galatea/settings.py
@@ -115,6 +115,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+                        "builtins": ["django.templatetags.i18n"], 
         },
     },
 ]
