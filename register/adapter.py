61118ea30e2a32064f85330c3463f9dd7274079e
pract <kimc570612@gmail.com>
2025-11-13 00:56:12 +0900

    'fefe'

--- register/adapter.py
+++ register/adapter.py
@@ -21,6 +21,57 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
         True이면 자동으로 회원가입 가능
         """
         return True
+
+    def populate_user(self, request, sociallogin, data):
+        """
+        OAuth 데이터로 사용자 객체를 채움
+        """
+        user = super().populate_user(request, sociallogin, data)
+
+        # email에서 username 자동 생성
+        if data.get('email'):
+            base_username = data['email'].split('@')[0]
+            username = base_username
+            counter = 1
+            from home.models import Users
+            while Users.objects.filter(username=username).exists():
+                username = f"{base_username}{counter}"
+                counter += 1
+            user.username = username
+            user.nickname = username
+
+        # phonenumber는 빈 문자열
+        user.phonenumber = ''
+
+        return user
+
+    def save_user(self, request, sociallogin, form=None):
+        """
+        소셜 로그인 후 사용자 저장 시 호출됨
+        form=None이면 자동으로 가입됨
+        """
+        user = super().save_user(request, sociallogin, form)
+
+        # 필수 필드 자동 채우기
+        if not user.username:
+            # email에서 username 생성
+            user.username = user.email.split('@')[0]
+            # 중복 방지
+            base_username = user.username
+            counter = 1
+            from home.models import Users
+            while Users.objects.filter(username=user.username).exclude(pk=user.pk).exists():
+                user.username = f"{base_username}{counter}"
+                counter += 1
+
+        if not user.phonenumber:
+            user.phonenumber = ''  # 빈 문자열로 설정
+
+        if not user.nickname:
+            user.nickname = user.username or 'user'
+
+        user.save()
+        return user
     def pre_social_login(self, request, sociallogin):
         print("=== pre_social_login triggered ===")
 
